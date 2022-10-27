f03c8eabcb1c6bf105b5079e1a8022db366273c7
PHRaulino <phraulino@outlook.com>
2022-10-26 22:46:41 -0300

    create components button checkbox textInput

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx'
-  ],
+  content: ["./src/**/*.tsx"],
   theme: {
     fontSize: {
       xs: 14,
@@ -10,24 +8,26 @@ module.exports = {
       md: 18,
       lg: 20,
       xl: 24,
-      '2xl': 32
-
+      "2xl": 32,
     },
     colors: {
-      'black': '#000000',
-      'gray-900': '#121214',
-      'gray-800': '#202024',
-      'gray-400': '#7C7C8A',
-      'gray-200': '#C4C4CC',
-      'gray-100': '#E1E1E6',
-      'cyan-500': '#81D8F7',
-      'cyan-300': '#9BE1FB',
+      transparent: 'transparent',
+      black: "#000",
+      white: "#FFF",
+      gray: {
+        900: "#121214",
+        800: "#202024",
+        400: "#7C7C8A",
+        200: "#C4C4CC",
+        100: "#E1E1E6",
+      },
+      cyan: { 500: "#81D8F7", 300: "#9BE1FB" },
     },
     extend: {
       fontFamily: {
-        sans: 'Inter, sans-serif'
-      }
+        sans: "Inter, sans-serif",
+      },
     },
   },
   plugins: [],
-}
+};
